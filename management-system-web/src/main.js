34c45860023b22899f78e06348ffb3c0f15eb7ac
strluck <s@strluck.com>
2021-11-22 15:03:28 +0800

    前端重置
    删除原有前端

--- management-system-web/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import installElementPlus from './plugins/element'
-import router from './router'
-import '@/assets/css/global.css'
-
-const app = createApp(App).use(router)
-installElementPlus(app)
-app.mount('#app')
