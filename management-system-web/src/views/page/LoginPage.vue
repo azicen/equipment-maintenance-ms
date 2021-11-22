2ed47ddba6014befe521ccf25bb206e8ac83c556
strluck <s@strluck.com>
2021-11-22 22:29:47 +0800

    前端 登录界面

--- management-system-web/src/views/page/LoginPage.vue
+++ management-system-web/src/views/page/LoginPage.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="login">
     <WordRotation/>
+    <LoginForm/>
   </div>
 </template>
 
 <script>
 import WordRotation from "@/views/component/data-display/WordRotation";
+import LoginForm from "@/views/component/form/LoginForm";
 
 export default {
   name: 'Login',
   components: {
-    WordRotation
+    WordRotation,
+    LoginForm,
   }
 }
 </script>
