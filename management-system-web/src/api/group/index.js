9260ec43384a256091676104993ddcffca4da63e
strluck <s@strluck.com>
2021-11-22 17:14:59 +0800

    前端 添加axios库

---
+++ management-system-web/src/api/group/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+//创建权限组信息
+const postGroup = form => axios.post('/api/v1/group', form).then(res => res.data);
+
+//读取权限组信息
+const getGroup = id => axios.get(`/api/v1/group/${id}`).then(res => res.data);
+
+//修改权限组信息
+const putGroup = (id, form) => axios.put(`/api/v1/group/${id}`, form).then(res => res.data);
+
+//删除权限组信息
+const delGroup = id => axios.delete(`/api/v1/group/${id}`).then(res => res.data);
+
+//读取权限组列表
+const getGroups = () => axios.get(`/api/v1/groups`).then(res => res.data);
+
+export {
+    postGroup,
+    getGroup,
+    putGroup,
+    delGroup,
+
+    getGroups,
+}
