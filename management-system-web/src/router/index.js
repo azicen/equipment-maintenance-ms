1d1f0ddf3e4241753f95cca06d1cfece75990fc6
strluck <s@strluck.com>
2021-11-22 20:43:06 +0800

    前端 login滚动标题

--- management-system-web/src/router/index.js
+++ management-system-web/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home.vue';
+import WordRotation from "@/views/component/data-display/WordRotation";
+import LoginPage from "@/views/page/LoginPage";
+import TestPage from "@/views/page/TestPage";
 
 const routes = [
   {
@@ -14,6 +17,27 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginPage,
+  },
+  {
+    path: '/test',
+    component: TestPage,
+    children: [
+      {
+        // 当 /user/:id/profile 匹配成功，
+        // UserProfile 会被渲染在 User 的 <router-view> 中
+        path: 'word-rotation',
+        component: WordRotation
+      },
+      // {
+      //   path: 'posts',
+      //   component: UserPosts
+      // }
+    ]
   }
 ]
 
---
+++ management-system-web/src/views/page/LoginPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    <WordRotation/>
+  </div>
+</template>
+
+<script>
+import WordRotation from "@/views/component/data-display/WordRotation";
+
+export default {
+  name: 'Login',
+  components: {
+    WordRotation
+  }
+}
+</script>
