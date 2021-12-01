afc12ee239d6ca6319fbbc4a7c8d3d687a7965aa
null122 <71970238+null122@users.noreply.github.com>
2021-12-01 20:03:02 +0800

    前端 登录api

--- management-system-web/src/components/form/LoginForm.vue
+++ management-system-web/src/components/form/LoginForm.vue
@@ -2,14 +2,14 @@
   <div class="login-form">
     <el-form ref="form" :model="form">
       <el-form-item label="账户">
-        <el-input v-model="form.account" placeholder="account"/>
+        <el-input v-model="form.id" placeholder="account"/>
       </el-form-item>
       <el-form-item label="密码">
         <el-input v-model="form.password" placeholder="password" show-password/>
       </el-form-item>
       <div class="login-form-button">
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">登录</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
         </el-form-item>
       </div>
     </el-form>
@@ -17,19 +17,33 @@
 </template>
 
 <script>
+import {UserLogin} from "@/api/login/userLogin";
+
 export default {
   data() {
     return {
       form: {
-        account: '',
+        id: '',
         password: '',
       },
     }
   },
-  methods: {
-    onSubmit() {
-      console.log('submit!')
-    },
+  setup(){
+    const userApi = new UserLogin()
+    function login(){
+      console.log("aaa")
+      userApi.Login(this.data().form.id,this.data().form.password).then(
+          (res)=>{
+            if(res.code===200){
+              alert("aaa")
+            }
+          console.log(res.data)
+        }
+      )
+    }
+    return {
+      login
+    }
   },
 }
 </script>
